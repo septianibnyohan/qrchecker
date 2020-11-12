389835ba54bed93fd229eb7ba99aa474d73645e1
unknown <septianibnyohan@gmail.com>
2020-11-13 01:39:50 +0700

    first commit

---
+++ Controllers/CheckerController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QRChecker.Models;
+
+namespace QRChecker.Controllers
+{
+    public class CheckerController : Controller
+    {
+        private readonly ILogger<CheckerController> _logger;
+
+        public CheckerController(ILogger<CheckerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
